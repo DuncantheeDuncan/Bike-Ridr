9e00b4ed961cba92fa403780886149b93481e9d9
DuncantheeDuncan <mthembup69@gmail.com>
2019-03-13 15:31:30 +0200

    first bike ride commit

---
+++ src/BikeRide.java
@@ -0,0 +1,19 @@
+import bicycles.Bicycle;
+
+public class BikeRide {
+    Bicycle bicycle = new Bicycle();
+    BikeRide bikeRide = new BikeRide(bicycle);
+    bikeRide.ride();
+
+    System.out.println(bikeRide.currentSpeed());
+
+
+    public void ride(){
+        this.bicycle.accelerate();
+        this.bicycle.accelerate();
+        this.bicycle.accelerate();
+        this.bicycle.brake();
+        this.bicycle.brake();
+        this.bicycle.accelerate();
+    }
+}
---
+++ tests/bicycles/BicycleTest.java
@@ -0,0 +1,11 @@
+package bicycles;
+
+import org.junit.jupiter.api.Test;
+
+public class BicycleTest {
+
+    @Test
+    public void shouldAccellerate() {
+        assertEquals(1,2);
+    }
+}
